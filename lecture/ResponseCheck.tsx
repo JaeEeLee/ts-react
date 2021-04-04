25487d1fb5a7efaf83e7241ce363be009e960b0f
LeeJaeEe <wodl0504@naver.com>
2021-04-04 18:47:14 +0900

    20210404 반응속도 체크 게임 완료

--- lecture/ResponseCheck.tsx
+++ lecture/ResponseCheck.tsx
@@ -13,49 +13,42 @@ const ResponseCheck = () => {
 	const startTime = useRef(0)
 	const endTime = useRef(0)
 
-	const onClickScreen = useCallback(() => {
-		const { floor, random } = Math
-		let text = 'now',
-			infoText = 'click now',
-			date = new Date().getTime(),
-			resultFunc = () => (prevResult: number[]) => {
-				return [...prevResult, endTime.current - startTime.current];
-			},
-			timeLogic = floor(random() * 1000) + 2000
+	const onClickScreen = () => {
+		let date: number = new Date().getTime()
 
 		if (state === 'waiting') {
-			timeout.current = window.setTimeout(() => { //window 가 없으면 timeout의 타입 에러 nodejs 꺼라서!
-				setState(text);
-				setMessage(infoText);
+			const { floor, random } = Math
+			let timeset: number = floor(random() * 1000) + 2000
+
+			timeout.current = window.setTimeout(() => {
+				setState('now');
+				setMessage('지금 클릭');
+
 				startTime.current = date
-			}, timeLogic)
+			}, timeset) // 2초~3초 랜덤
 
-			text = 'ready'
-			infoText = 'Click when color turns green'
+			setState('ready');
+			setMessage('초록색이 되면 클릭하세요.');
 		}
-
-		if (state === 'ready') {
-			clearTimeout(timeout.current!) //? 왜 뒤에 느낌표를 주지?
-			text = 'waiting'
-			infoText = 'Calm down. and click when color turns green'
+		if (state === 'ready') { // 성급하게 클릭
+			clearTimeout(timeout.current!);//window 가 없으면 timeout의 타입 에러 nodejs 꺼라서!
+			setState('waiting');
+			setMessage('너무 성급하시군요! 초록색이 된 후에 클릭하세요.');
 		}
-
-		if (state === 'now') {
+		if (state === 'now') { // 반응속도 체크
 			endTime.current = date
-			text = 'waiting'
-			infoText = 'click and start'
+			setState('waiting');
+			setMessage('클릭해서 시작하세요.');
+			setResult(prevResult => {
+				return [...prevResult, endTime.current - startTime.current];
+			})
 		}
-
-		setState(text)
-		setMessage(infoText)
-
-		state === 'now' && resultFunc()
-	}, [state, message])
-
+	}
+	
 	//매개변수 안잡혀서 타이핑 불필요
 	const onReset = useCallback(() => {
 		setResult([]);
-	},[])
+	}, [])
 
 	const renderAverage = () => {
 		return result.length !== 0 &&
@@ -93,51 +86,51 @@ const ResponseCheck = () => {
   const endTime = useRef(0);
 
   const onClickScreen = () => {
-    if (state === 'waiting') {
-      timeout.current = window.setTimeout(() => {
-        setState('now');
-        setMessage('지금 클릭');
-        startTime.current = new Date().getTime();
-      }, Math.floor(Math.random() * 1000) + 2000); // 2초~3초 랜덤
-      setState('ready');
-      setMessage('초록색이 되면 클릭하세요.');
-    } else if (state === 'ready') { // 성급하게 클릭
-      clearTimeout(timeout.current!);
-      setState('waiting');
-      setMessage('너무 성급하시군요! 초록색이 된 후에 클릭하세요.');
-    } else if (state === 'now') { // 반응속도 체크
-      endTime.current = new Date().getTime();
-      setState('waiting');
-      setMessage('클릭해서 시작하세요.');
-      setResult((prevResult) => {
-        return [...prevResult, endTime.current - startTime.current];
-      });
-    }
+	if (state === 'waiting') {
+	  timeout.current = window.setTimeout(() => {
+		setState('now');
+		setMessage('지금 클릭');
+		startTime.current = new Date().getTime();
+	  }, Math.floor(Math.random() * 1000) + 2000); // 2초~3초 랜덤
+	  setState('ready');
+	  setMessage('초록색이 되면 클릭하세요.');
+	} else if (state === 'ready') { // 성급하게 클릭
+	  clearTimeout(timeout.current!);
+	  setState('waiting');
+	  setMessage('너무 성급하시군요! 초록색이 된 후에 클릭하세요.');
+	} else if (state === 'now') { // 반응속도 체크
+	  endTime.current = new Date().getTime();
+	  setState('waiting');
+	  setMessage('클릭해서 시작하세요.');
+	  setResult((prevResult) => {
+		return [...prevResult, endTime.current - startTime.current];
+	  });
+	}
   };
   const onReset = () => {
-    setResult([]);
+	setResult([]);
   };
 
   const renderAverage = () => {
-    return result.length === 0
-      ? null
-      : <>
-        <div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
-        <button onClick={onReset}>리셋</button>
-      </>
+	return result.length === 0
+	  ? null
+	  : <>
+		<div>평균 시간: {result.reduce((a, c) => a + c) / result.length}ms</div>
+		<button onClick={onReset}>리셋</button>
+	  </>
   };
 
   return (
-    <>
-      <div
-        id="screen"
-        className={state}
-        onClick={onClickScreen}
-      >
-        {message}
-      </div>
-      {renderAverage()}
-    </>
+	<>
+	  <div
+		id="screen"
+		className={state}
+		onClick={onClickScreen}
+	  >
+		{message}
+	  </div>
+	  {renderAverage()}
+	</>
   );
 };
 
